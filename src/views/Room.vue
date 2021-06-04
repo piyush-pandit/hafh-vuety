32b5f7815dc6c3f03675ed34d2a640d0bdb57b19
chhaya <chhptl8@gmail.com>
2021-06-04 19:17:26 +0530

    changes

--- src/views/Room.vue
+++ src/views/Room.vue
@@ -1,17 +1,76 @@
 <template>
   <div>
-    <v-card class="mx-auto" max-width="400">
+    <v-card class="mx-auto" max-width="100%">
       <v-img
         class="main"
         height="200px"
         src="http://d2u4q3iydaupsp.cloudfront.net/M9YO50StNO9wcaIKVYLMIAVeaOrWxAHoEXoIO0K1D9hMa6RlXDkuExDgDwGnn1NTP70WO5k6SoYUhqA2jEzvSp5Fj0jmHNjAWq11rOt0OA59j6pLWSEK6jEWMrGxtiG2"
       >
-        <v-card-title>My Rv</v-card-title>
+        <v-fab-transition>
+          <v-btn
+            class="btn"
+            style="color: white"
+            top
+            left
+            fixed
+            small
+            light
+            href="/rv"
+          >
+            <v-icon dark left> mdi-arrow-left </v-icon> My Rv
+          </v-btn>
+        </v-fab-transition>
       </v-img>
 
-      <v-list>
+      <v-list style="padding-top: 30px">
+        <!-- <template v-for="room in rooms">
+          <v-list-item :key="room._id">
+            <v-list-item-avatar>
+              <v-img
+                :
+                src="https://st.depositphotos.com/1203257/1763/i/950/depositphotos_17637335-stock-photo-yellowstone-rv-trip.jpg"
+              ></v-img>
+            </v-list-item-avatar>
+
+            <v-list-item-content style="text-align: left; margin-left: 15px">
+              <strong
+                ><router-link
+                  :to="{ name: 'Post', params: { roomId: room._id } }"
+                  tag="v-btn"
+                >
+                  <v-list-item-title style="text-align: left; margin-left: 5px"
+                    >{{ room.name }}
+                  </v-list-item-title>
+                </router-link>
+                ></strong
+              >
+            </v-list-item-content>
+            <router-link
+              :to="{
+                name: 'RoomEdit',
+                params: { roomId: room._id, rvId: rvId },
+              }"
+              tag="v-btn"
+            >
+              <v-btn class="mx-2" fab dark small color="cyan">
+                <v-icon dark> mdi-pencil </v-icon>
+              </v-btn>
+            </router-link>
+            <v-btn
+              @click="deleteRoom(room._id)"
+              class="mx-2"
+              fab
+              dark
+              small
+              color="cyan"
+            >
+              <v-icon dark> mdi-delete </v-icon>
+            </v-btn>
+          </v-list-item>
+        </template> -->
+
         <v-list-item v-for="room in rooms" v-bind:key="room._id">
-          <v-list-item-avatar size="56px">
+          <v-list-item-avatar size="40px">
             <img
               alt="Avatar"
               src="https://st.depositphotos.com/1203257/1763/i/950/depositphotos_17637335-stock-photo-yellowstone-rv-trip.jpg"
@@ -23,7 +82,9 @@
               :to="{ name: 'Post', params: { roomId: room._id } }"
               tag="v-btn"
             >
-              <v-list-item-title>{{ room.name }} </v-list-item-title>
+              <v-list-item-title style="text-align: left; margin-left: 5px"
+                ><strong>{{ room.name }}</strong>
+              </v-list-item-title>
             </router-link>
           </v-list-item-content>
           <router-link
